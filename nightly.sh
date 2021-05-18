ad9d7d0308c73404e0b79592bcc40beaa82618a3
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-05-18 11:05:20 +0530

    update nightly script

--- nightly.sh
+++ nightly.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #add local path to crontab path
-PATH=$PATH:/home/shubham/.local/bin:/home/shubham/.opam/4.10.0+multicore/bin:/home/shubham/.local/bin:/home/shubham/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+#PATH=$PATH:/home/shubham/.local/bin:/home/shubham/.opam/4.10.0+multicore/bin:/home/shubham/.local/bin:/home/shubham/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
 #initialize the date and time and create sequential and parallel directories
 timestamp=$(date +%Y%m%d_%H%M%S)
-mkdir -p /home/shubham/sandmark-nightly/$timestamp/sequential
-mkdir -p /home/shubham/sandmark-nightly/$timestamp/parallel
+mkdir -p /home/shubham/sandmark-nightly/sequential/$timestamp
+mkdir -p /home/shubham/sandmark-nightly/parallel/$timestamp
 
 #get the latest sandmark pull
 cd /home/shubham/sandmark/
@@ -17,21 +17,24 @@ make clean
 TAG='"macro_bench"' make run_config_filtered.json
 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+stock.bench
 RUN_CONFIG_JSON=run_config_filtered.json make ocaml-versions/4.12.0+domains+effects.bench
+
 cp -a /home/shubham/sandmark/_results/*.bench /home/shubham/sandmark-nightly/sequential/$timestamp
-make clean
+rm -rf _results/
 
 #parallel benchmarks
 TAG='"macro_bench"' make multicore_parallel_run_config_filtered.json
 RUN_BENCH_TARGET=run_orunchrt \
-	        BUILD_BENCH_TARGET=multibench_parallel \
-		        RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
-			        make ocaml-versions/4.12.0+domains.bench
+	BUILD_BENCH_TARGET=multibench_parallel \
+	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
+	make ocaml-versions/4.12.0+domains.bench
+
 RUN_BENCH_TARGET=run_orunchrt \
-	        BUILD_BENCH_TARGET=multibench_parallel \
-		        RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
-			        make ocaml-versions/4.12.0+domains+effects.bench
+	BUILD_BENCH_TARGET=multibench_parallel \
+	RUN_CONFIG_JSON=multicore_parallel_run_config_filtered.json \
+	make ocaml-versions/4.12.0+domains+effects.bench
+
 cp -a /home/shubham/sandmark/_results/*.bench /home/shubham/sandmark-nightly/parallel/$timestamp
-make clean
+rm -rf _results/
 
 #push to sandmark-nightly
 cd /home/shubham/sandmark-nightly
