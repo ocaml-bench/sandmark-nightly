b9b03fa0017fd39dc1934d406a8aaf716e3eb6c6
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-09-22 05:11:36 +0530

    update docker
    update setup_aws.sh

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Check if SKEL=/etc/skel in /etc/default/useradd is uncommented and then proceed to execute the bash script
 
-sudo -E pip install nested_dict pandas seaborn
+#pip3 install nested_dict pandas seaborn
 
-git clone https://github.com/shubhamkumar13/sandmark-nightly.git
+git clone https://github.com/ocaml-bench/sandmark-nightly.git
 
-sudo mkdir -p /srv/data/
-sudo cp -a sandmark-nightly /srv/data
+mkdir -p /srv/data/
+cp -a $(pwd)/sandmark-nightly /srv/data
 
-sudo ln -s /srv/data/sandmark-nightly/ /etc/skel/sandmark-nightly
+ln -s /srv/data/sandmark-nightly/ /etc/skel/sandmark-nightly
 
-sudo cp /srv/data/sandmark-nightly/notebooks/*.ipynb /etc/skel
+cp /srv/data/sandmark-nightly/notebooks/*.ipynb /etc/skel
 
-cp sandmark-nightly/scripts/update.sh .
+cp $(pwd)/sandmark-nightly/scripts/update.sh .
 
 chmod +x update.sh
 
