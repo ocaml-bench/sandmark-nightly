ocaml-bench/sandmark-nightly

commit 76cfc5d7a590772a7967ddf27f2cca692cbbbb7c

Add hostname to the automated commit message and slack notifications
Closes #74

scripts/copy-successful-builds.sh
@@ -15,6 +15,8 @@ git fetch "${GIT_REMOTE}" main
 
 LAST_COMMIT_FILES=$(git diff-tree --no-commit-id --name-only -r "${GIT_REMOTE}/testing" --diff-filter=d)
 
+HOSTNAME=$(echo "${LAST_COMMIT_FILES}" | cut -d "/" -f 2 | sort -u | head -n 1)
+
 CHANGED_DIRS=$(for each in ${LAST_COMMIT_FILES}; do
                    dirname "${each}"
                done | sort -u)
@@ -43,13 +45,12 @@ if [ ${SUCCESSFUL_BUILDS} -ge 1 ]; then
     git config user.email "puneeth+sandmark@tarides.com"
     git config user.name "Sandmark Nightly Bot"
     git diff --name-only --cached | grep -qoP "." && \
-        git commit -m "Automated commit for successful benchmarks in ${TESTING_COMMIT}"
-    git push "${GIT_REMOTE}" main
+        git commit -m "Auto-copy successful results from ${TESTING_COMMIT} (${HOSTNAME})"
     MAIN_COMMIT=$(git rev-parse HEAD)
 fi
 
 git checkout "${GIT_REMOTE}/testing"
 
-"$(dirname "${0}")/slack-notify-build-status.sh" "${CHANGED_DIRS}" "${TESTING_COMMIT}" "${MAIN_COMMIT:-}"
+"$(dirname "${0}")/slack-notify-build-status.sh" "${CHANGED_DIRS}" "${TESTING_COMMIT}" "${MAIN_COMMIT:-}" "${HOSTNAME}"
 
 git checkout "${CURRENT_BRANCH}"

scripts/slack-notify-build-status.sh
@@ -10,6 +10,7 @@ function commit_url {
 CHANGED_DIRS=$1
 TESTING_COMMIT=$2
 MAIN_COMMIT=$3
+HOSTNAME=$4
 
 for dir in ${CHANGED_DIRS}; do
     BENCH_FILE=$(find "${dir}" -name "*.summary.bench" -type f | head -n 1)
@@ -26,7 +27,7 @@ if [ -n "${PASSED_BUILDS:-}" ]; then
                         \"type\": \"header\",
                         \"text\": {
                                 \"type\": \"plain_text\",
-                                \"text\": \"Successful builds\"
+                                \"text\": \"Successful builds on ${HOSTNAME}\"
                         }
                 },
                 {
@@ -50,7 +51,7 @@ if [ -n "${FAILED_BUILDS:-}" ]; then
                         \"type\": \"header\",
                         \"text\": {
                                 \"type\": \"plain_text\",
-                                \"text\": \"Failed builds\"
+                                \"text\": \"Failed builds on ${HOSTNAME}\"
                         }
                 },
                 {

commit cb788659f231d341f1ad3902e63687bf1a179d29

Fix accidentally deleted push in successful results copy script
76cfc5d7a590772a7967ddf27f2cca692cbbbb7c accidentally deleted the `git push`
command, which broke the copying of successful builds from testing to main.

scripts/copy-successful-builds.sh
@@ -51,6 +51,7 @@ if [ ${SUCCESSFUL_BUILDS} -ge 1 ]; then
     git config user.name "Sandmark Nightly Bot"
     git diff --name-only --cached | grep -qoP "." && \
         git commit -m "Auto-copy successful results from ${TESTING_COMMIT} (${HOSTNAME})"
+    git push "${GIT_REMOTE}" main
     MAIN_COMMIT=$(git rev-parse HEAD)
 fi
 