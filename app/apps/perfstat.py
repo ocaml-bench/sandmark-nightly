7211ff302d6abb4dc2cee81af617f327aae98348
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-10-11 19:00:01 +0530

    Use pivot and melt to normalize the data instead of groupby
    The groupby crashes when new metrics are introduced and the fix here is similar
    to the fix in 3a8d0926a46cb6dc702357d7cfedb6ae5d64c5bb

--- app/apps/perfstat.py
+++ app/apps/perfstat.py
@@ -63,29 +63,24 @@ def extract(field, line, row):
 
 def normalise(df, variant, topic, additionalTopics=[]):
     df = add_display_name(df, variant, topic)
-    df = df.sort_values(["name", "variant"])
-    grouped = df.filter(
-        items=["name", topic, "variant", "display_name"] + additionalTopics
-    ).groupby("variant")
-    ndata_frames = []
-    for group in grouped:
-        (v, data) = group
-        if v != variant:
-            data["b" + topic] = grouped.get_group(variant)[topic].values
-            data[["n" + topic]] = data[[topic]].div(
-                grouped.get_group(variant)[topic].values, axis=0
-            )
-            for t in additionalTopics:
-                data[[t]] = grouped.get_group(variant)[t].values
-            ndata_frames.append(data)
-    if ndata_frames:
-        df = pd.concat(ndata_frames)
-        return df
-    else:
+    items = ["name", topic, "variant", "display_name"] + additionalTopics
+    df_filtered = df.filter(items=items)
+    try:
+        df_pivot = df_filtered.reset_index().pivot(
+            index="name", columns="variant", values=[topic]
+        )
+    except ValueError:
         st.warning(
             "Variants selected are the same, please select different variants to generate a normalized graph"
         )
         return pd.DataFrame()
+    baseline_column = (topic, variant)
+    select_columns = [c for c in df_pivot.columns if c != baseline_column]
+    normalised = df_pivot.div(df_pivot[baseline_column], axis=0)[select_columns]
+    normalised = normalised.melt(
+        col_level=1, ignore_index=False, value_name="n" + topic
+    ).reset_index()
+    return pd.merge(normalised, df_filtered, on=["name", "variant"])
 
 
 def plot_normalised(df, variant, topic):
