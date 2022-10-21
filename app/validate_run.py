7a70252858780657d247d341f19cd2ff12f755bb
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-10-21 15:32:11 +0530

    Refactor validate_run to be used in streamlit page

--- app/validate_run.py
+++ app/validate_run.py
@@ -5,25 +5,29 @@ def bench_file_has_content(path, n=1):
     """Ensure the bench file in a directory has at least n lines of content."""
     bench_files = list(path.glob("**/*.bench"))
     if not bench_files:
-        print("Could not find bench file")
-        return False
+        return False, None
     bench_file = bench_files[0]
-    print(f"bench: {bench_file}")
     with open(bench_file) as f:
         lines = f.readlines()
-    return len(lines) > n
+    return len(lines) > n, bench_file
 
 
 def log_has_no_errors(path):
     log_files = list(path.glob("**/*.log"))
     if not log_files:
         print("Could not find log file")
-        return False
+        return False, None
     log_file = log_files[0]
-    print(f"log: {log_file}")
     with open(log_file) as f:
         log_text = f.read()
-    return "Error:" not in log_text
+    return "Error:" not in log_text, log_file
+
+
+def is_valid(path):
+    bench_status, bench_file = bench_file_has_content(path)
+    log_status, log_file = log_has_no_errors(path)
+    status = bench_status and log_status
+    return dict(status=status, log_file=log_file, bench_file=bench_file)
 
 
 if __name__ == "__main__":
@@ -36,9 +40,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     path = Path(args.path)
-    bench_status = bench_file_has_content(path)
-    log_status = log_has_no_errors(path)
-    if bench_status and log_status:
-        sys.exit(0)
+    data = is_valid(path)
+    log_file = data["log_file"]
+    if not log_file:
+        print("Could not file log file")
     else:
-        sys.exit(1)
+        print(f"log: {log_file}")
+
+    bench_file = data["bench_file"]
+    if not bench_file:
+        print("Could not file bench file")
+    else:
+        print(f"bench: {bench_file}")
+
+    sys.exit(not data["status"])
