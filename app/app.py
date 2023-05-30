bb6b2b4ca34a108dd3a50858d4b5f04c8eb7f246
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-05-30 14:01:28 +0530

    Move app title, function and saved session keys to a single config

--- app/app.py
+++ app/app.py
@@ -1,14 +1,9 @@
+import importlib
+
 import streamlit as st
 
-from apps import (
-    index,
-    instrumented_pausetimes_parallel,
-    instrumented_pausetimes_sequential,
-    parallel_benchmarks,
-    perfstat,
-    sequential_benchmarks,
-)
 from apps.utils import write_params_to_session
+from config import config
 from multiapp import MultiApp
 
 st.set_page_config(page_title="Sandmark Nightly", page_icon="🐫", layout="wide")
@@ -16,13 +11,10 @@ st.set_page_config(page_title="Sandmark Nightly", page_icon="🐫", layout="wide
 params = st.experimental_get_query_params()
 app = MultiApp(params)
 
-# Add all your application here
-app.add_app("Home", index.app)
-app.add_app("Sequential Benchmarks", sequential_benchmarks.app)
-app.add_app("Parallel Benchmarks", parallel_benchmarks.app)
-app.add_app("Perfstat Output", perfstat.app)
-app.add_app("Instrumented Pausetimes Sequential", instrumented_pausetimes_sequential.app)
-app.add_app("Instrumented Pausetimes Parallel", instrumented_pausetimes_parallel.app)
+for title, data in config.items():
+    name = data["module"]
+    module = importlib.import_module(f"apps.{name}")
+    app.add_app(title, module.app)
 
 # The main app
 params = st.experimental_get_query_params()
